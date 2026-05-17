0f17b21e3634adcbded71de97283bc4f5c1bb59d
Chris Prieto <ionz149@gmail.com>
2026-05-17 09:17:48 +0000

    adding event data type, updating data object aray

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,29 +1,60 @@
-export const events = [
-    {
-        id: "pyramids",
-        title: "Great Pyramid of Giza",
-        startYear: -2580,
-        endYear: -2560,
-    },
+import type { TimelineEvent } from "../types/event";
 
-    {
-        id: "roman-empire",
-        title: "Roman Empire",
-        startYear: -27,
-        endYear: 476,
-    },
+export const events: TimelineEvent[] = [
+	{
+		id: "pyramids",
+		title: "Great Pyramid of Giza",
+		startYear: -2580,
+		endYear: -2560,
+		category: "wonders",
+		region: "egypt",			
+	},
 
-    {
-        id: "american-revolution",
-        title: "American Revolution",
-        startYear: 1765,
-        endYear: 1783,
-    },
+	{
+		id: "roman-empire",
+		title: "Roman Empire",
+		startYear: -27,
+		endYear: 476,
+		category: "empires",
+		region: "rome",		
+	},
 
-    {
-        id: "world-war-2",
-        title: "World War II",
-        startYear: 1939,
-        endYear: 1945,
-    },
+	{
+		id: "american-revolution",
+		title: "American Revolution",
+		startYear: 1765,
+		endYear: 1783,
+		category: "warfare",
+		region: "united-states",
+	},  	
+	{
+		id: "ww1",
+		title: "World War I",
+		startYear: 1914,
+		endYear: 1918,
+		category: "warfare",
+		region: "global",
+	},
+	{
+		id: "ww2",
+		title: "World War II",
+		startYear: 1939,
+		endYear: 1945,
+		category: "warfare",
+		region: "global",
+	},
+	{
+		id: "einstein_relativity",
+		title: "General Relativity Published",
+		startYear: 1915,
+		endYear: 1915,
+		category: "science",
+	},
+	{
+		id: "apollo_11",
+		title: "Apollo 11 Moon Landing",
+		startYear: 1969,
+		endYear: 1969,
+		category: "science",
+	},
 ];
---
+++ src/types/event.ts
@@ -0,0 +1,19 @@
+export type EventCategory =
+  | "warfare"
+  | "science"
+  | "politics"
+  | "culture"
+  | "economy"
+  | "technology"
+  | "empires"
+  | "wonders"
+  | "other";
+
+export interface TimelineEvent {
+  id: string;
+  title: string;
+  startYear: number;
+  endYear: number;
+  category: EventCategory;
+  region?: string;
+}
